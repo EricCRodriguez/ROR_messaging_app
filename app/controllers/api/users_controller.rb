b0d61ca80c09d439001ca70a093e64fb0e5d3cf3
EricCRodriguez <ecrod12@gmail.com>
2021-06-22 16:41:11 -0500

    added user contoller/index/show.json

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,34 @@
 class Api::UsersController < ApplicationController
+def index
+    @users = User.all
+    render "index.json.jb"
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+    )
+    @user.save
+    render "show.json.jb"
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name] || @user.name
+    @user.email = params[:email] || @user.email
+    @user.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    user.destroy
+    render json: {message: "User successfully destroyed."}
+  end
 end
