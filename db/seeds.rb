51fd4acb019eceab74a9254b7adb1aef5b9b8d47
EricCRodriguez <ecrod12@gmail.com>
2021-06-22 15:49:45 -0500

    user db seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
- Photo.create(name: "Eric", email: eric@email.com)
+ User.create!(name: "Eric", email: "eric@email.com")
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
