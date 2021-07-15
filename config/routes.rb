89dd2c9405827443a1d211abc4a6bb4f15b2b643
EricCRodriguez <ecrod12@gmail.com>
2021-07-15 14:28:53 -0500

    added devise for routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-   namespace :api do
-    get "/users" => "users#index"
-    post "/users" => "users#create"
-    get "/users/:id" => "users#show"
-    patch "/users/:id" => "users#update"
-    delete "/users/:id" => "users#destroy" 
-    
+   devise_for :users
+
+  resources :conversations do
+    resources :messages
   end
+
+  root to: 'main#index'
 end
